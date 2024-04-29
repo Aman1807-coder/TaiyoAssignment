dada779d3cd8147fcdb5dbb34ddeda2f0dca3105
Aman <amansrivastav1825@gmail.com>
2024-04-29 10:55:21 +0530

    final commit

--- src/components/chartsandmaps/chart.tsx
+++ src/components/chartsandmaps/chart.tsx
@@ -22,120 +22,48 @@ const Chart = () => {
   });
 
   interface dataInterface {
-    month: string;
-    2020: number;
-    2021: number;
-    2022: number;
-    2023: number;
+    day: string;
+    cases: number;
+    deaths: number;
+    recovered: number;
   }
+
+  console.log(data)
 
   const filteredData: dataInterface[] = [];
 
-  let year20 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let year21 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let year22 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let year23 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
   if (data) {
-    const obj = data.cases;
+    const objCases = data.cases;
+    const objRecovered = data.recovered;
+    const objDeaths = data.deaths;
 
-    for (const key in obj) {
-      const separatedArray = key.split("/");
-      const month = Number(separatedArray[0]);
-      const year = separatedArray[2];
-      const cases = obj[key];
-
-      if (year === "20") year20[month - 1] += cases;
-      else if (year === "21") year21[month - 1] += cases;
-      else if (year === "22") year22[month - 1] += cases;
-      else if (year === "23") year23[month - 1] += cases;
-    }
-
-    for (let i = 0; i < 12; ++i) {
-      let monthName = "";
-      switch (i) {
-        case 0:
-          monthName = "Jan";
-          break;
-
-        case 1:
-          monthName = "Feb";
-          break;
-
-        case 2:
-          monthName = "Mar";
-          break;
-
-        case 3:
-          monthName = "Apr";
-          break;
-
-        case 4:
-          monthName = "May";
-          break;
-
-        case 5:
-          monthName = "Jun";
-          break;
-
-        case 6:
-          monthName = "Jul";
-          break;
-
-        case 7:
-          monthName = "Aug";
-          break;
-
-        case 8:
-          monthName = "Sept";
-          break;
-
-        case 9:
-          monthName = "Oct";
-          break;
-
-        case 10:
-          monthName = "Nov";
-          break;
-
-        case 11:
-          monthName = "Dec";
-          break;
-      }
-
-      let d = 31;
-      if (i === 1) d = 28;
-      else if (i === 3 || i === 5 || i === 8 || i === 10) d = 30;
-
-      year20[i] = year20[i] / d;
-      year21[i] = year21[i] / d;
-      year22[i] = year22[i] / d;
-      year23[i] = year23[i] / d;
+    for (const day in objCases) {
+      const cases = objCases[day]/1000000;
+      const recovered = objRecovered[day]/1000000;
+      const deaths = objDeaths[day]/1000000;
 
       filteredData.push({
-        month: monthName,
-        2020: year20[i]/1000000,
-        2021: year21[i]/1000000,
-        2022: year22[i]/1000000,
-        2023: year23[i]/1000000,
+        day: day,
+        cases: cases,
+        recovered: recovered,
+        deaths: deaths
       });
     }
   }
 
   return (
-    <div style={{left: "48vh", position: "absolute", top: "80vh"}} 
+    <div style={{left: "48vh", position: "absolute", top: "80vh"}}
     className="bg-stone-300">
       <p className="m-5">Y-axis represents Cases in Millions</p>
       <LineChart width={1000} height={500} data={filteredData}>
-        <XAxis dataKey="month" />
+        <XAxis dataKey="day" />
         <YAxis />
         <Tooltip />
         <Legend />
         <CartesianGrid stroke="#f5f5f5" />
-        <Line type="monotone" dataKey="2020" stroke="#ff7300" />
-        <Line type="monotone" dataKey="2021" stroke="#387908" />
-        <Line type="monotone" dataKey="2022" stroke="#3b82f6" />
-        <Line type="monotone" dataKey="2023" stroke="#8b5cf6" />
+        <Line type="monotone" dataKey="cases" stroke="#ff7300" />
+        <Line type="monotone" dataKey="recovered" stroke="#387908" />
+        <Line type="monotone" dataKey="deaths" stroke="#3b82f6" />
       </LineChart>
     </div>
   );
